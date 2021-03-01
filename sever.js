be64a00f485348566b4196f30e8b0be3a511b0b2
h976 <hanaaelkorri@gmail.com>
2021-03-01 22:28:30 +0100

    second commit

--- sever.js
+++ sever.js
@@ -72,6 +72,6 @@ io.on('connection', socket => {
     });
   });
 
-const PORT = process.env.PORT || 8888;
+const PORT = process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
