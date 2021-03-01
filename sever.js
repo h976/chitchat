a5eb273a7f7c1e9ef555d6b1872578fbf19988f1
h976 <hanaaelkorri@gmail.com>
2021-03-01 13:19:11 +0100

    deploying to the server

--- sever.js
+++ sever.js
@@ -72,6 +72,6 @@ io.on('connection', socket => {
     });
   });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8888;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
