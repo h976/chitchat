d56206adda2dc183a225fdd15506be41b72b09b0
h976 <hanaaelkorri@gmail.com>
2021-03-02 01:23:26 +0100

    wooooooooooork

--- server.js
+++ server.js
@@ -72,6 +72,6 @@ io.on('connection', socket => {
     });
   });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8888;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
